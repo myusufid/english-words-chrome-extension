66473c9c16359ac835fb574d38b273cecf8c5efe
M Yusuf <muhammadyusuf931@gmail.com>
2022-10-29 22:54:29 +0700

    setup extension chrome

---
+++ js/jquery/app.js
@@ -0,0 +1,29 @@
+    const getDictionary = async function(a, b) {
+        let response = await fetch('./dictionary.json');
+        return response.json();
+    }
+
+
+    $(function(){
+        
+        function showDictionary(){
+            getDictionary().then((data) => {
+            var dictionary = data
+            var number = Math.floor(Math.random() * dictionary.length)
+            var item = dictionary[number];
+            $("#title").html(item.title)
+            $("#description").html(item.description)
+            $("#image").attr('src', item.image)
+            });
+        }
+
+        showDictionary()
+
+        setInterval(function(){
+            $("#title").html()
+            $("#description").html()
+            $("#image").attr('src', null)
+            showDictionary()
+        }, 5000);
+
+    })
